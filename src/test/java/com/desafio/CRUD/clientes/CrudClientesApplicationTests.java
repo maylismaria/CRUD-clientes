be106de7ad10c5043f0b64ceab57b1be574d91e4
Maylis Maria <maylis.maria15@gmail.com>
2025-09-10 19:42:47 -0300

    primeiro commit do projeto

---
+++ src/test/java/com/desafio/CRUD/clientes/CrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.CRUD.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
